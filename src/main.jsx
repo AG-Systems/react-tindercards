59d8eb863992404956b04f91977777e541d58ba0
Auriga <AG-Systems@users.noreply.github.com>
2017-01-04 01:27:25 -0800

    Refactored the code to make it more reactlike

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,43 @@
-  
+  var counter = 1;
   const divStyle = {
     width: 350,
     margin: 0
       };
+    function nextcard(checker)
+    {
+      $("." + checker).remove();
+      checker++;
+      $("."+checker).show();
+    }
     var List = React.createClass({
+        like: function () {
+            nextcard(counter);
+            console.log("like");
+            counter += 1;
+        },
+        dislike: function () {
+            nextcard(counter);
+            console.log("dislike");
+            counter += 1;
+        },
         render: function() {
+          
           return (
-            <div>
-              {this.props.list.map( post =>
+            <ul>
+              {this.props.list.map(( post,index )=>
                 <div id="main" style={divStyle} className={post.id}>
-                    <div className="card">
-                      <img className="card-img-top" src="https://placehold.it/350x150" alt="Card image cap" />
-                      <div className="card-block">
-                        <h4 className="card-title">{ post.title }</h4>
-                        <p className="card-text">{ post.body }</p>
-                        <button type='button' className="btn btn-primary"> Yes, I'm Interested </button>
-                        <button type='button' className="btn btn-danger"> No, Skip </button>
+                      <div className="card">
+                        <img className="card-img-top" src="https://placehold.it/350x150" alt="Card image cap" />
+                        <div className="card-block">
+                          <h4 className="card-title">{ post.title }</h4>
+                          <p className="card-text">{ post.body }</p>
+                          <button type='button' onClick={this.like} className="btn btn-primary"> Yes, I'm Interested </button>
+                          <button type='button' onClick={this.dislike} className="btn btn-danger"> No, Skip </button>
+                        </div>
                       </div>
-                    </div>
-                </div>
-              )}
-            </div>
+                  </div>
+              )} 
+            </ul>
           )
         }
       });
@@ -37,6 +54,30 @@
                   "title": "Person2",
                   "body": "bio2",
                   "id":"2"
+                },
+                ,
+                {
+                  "title": "Person3",
+                  "body": "bio3",
+                  "id":"3"
+                },
+                ,
+                {
+                  "title": "Person4",
+                  "body": "bio4",
+                  "id":"4"
+                },
+                ,
+                {
+                  "title": "Person5",
+                  "body": "bio5",
+                  "id":"5"
+                },
+                ,
+                {
+                  "title": "Person6",
+                  "body": "bio6",
+                  "id":"6"
                 }
               ]
             }
